ab833c36e21e978aca9a11a57e403597e20e4d72
Andro Sten <andro-sten.kriisa@voco.ee>
2022-10-21 11:41:54 +0300

    Arvutamine

---
+++ Arvutamine.py
@@ -0,0 +1,60 @@
+num_a = 5
+num_b = 2
+sum = num_a + num_b
+print(f"{num_a} + {num_b} = {sum}")
+print("-----------------------------------------")
+
+difference = num_a - num_b
+print(f"){num_a} - {num_b} = {difference}")
+print("-----------------------------------------")
+
+division = num_a // num_b
+print(f"){num_a} // {num_b} = {division}")
+print("-----------------------------------------")
+
+multiply_numbers = num_a * num_b
+power = num_a ** num_b
+remainder = num_a % num_b
+print(f"){num_a} % {num_b} = {remainder}")
+print("-----------------------------------------------")
+
+average = (num_a + num_b) / 2
+print(f")({num_a} + {num_b}) / {2} = {average}")
+print ("-------------------------------------------------")
+
+radius = 3.5
+import math
+pindala = radius * radius * math.pi
+print(f"){radius} * {radius} * math.pi = {pindala}")
+print(round(38.48451000647496, 2))
+circle_area = round(38.48451000647496, 2)
+print("------------------------------------------------")
+
+side_length =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
