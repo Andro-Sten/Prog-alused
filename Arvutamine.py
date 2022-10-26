bb23604d69aa1372dc859d760c3bbe3fe6cd3f5d
Andro Sten <andro-sten.kriisa@voco.ee>
2022-10-26 11:40:55 +0300

    Arvutamine

--- Arvutamine.py
+++ Arvutamine.py
@@ -30,31 +30,64 @@ print(round(38.48451000647496, 2))
 circle_area = round(38.48451000647496, 2)
 print("------------------------------------------------")
 
-side_length =
+side_length = 5
+triangle_area = (math.sqrt(3) / 4) * side_length ** 2
+print(triangle_area)
+triangle_area = round(triangle_area)
+print(triangle_area)
+print(f"Triangle area is {triangle_area} if side length is {side_length}")
+print("-------------------------------------------------------")
 
+a = 1
+b = 5
+c = 6
+discriminant = b ** 2 - 4 * a * c
+print(discriminant)
+print(f"Numbers {a} and {b} and {c} discriminant is {discriminant}")
+print("--------------------------------------------------------------")
+a = 2
+b = 4
 
+c = math.sqrt(a ** 2 + b ** 2)
+print(c)
+print(f"Numbers {a} and {b} hypotenuse is {c}")
+print(f"----------------------------------------------------------")
+a = 2
+c = 4
 
+b = math.sqrt(c ** 2 - a ** 2)
+print (b)
+print(f"Numbers {a} and {c} cathetus is {b}")
+print(f"-------------------------------------------------------------------------")
 
+seconds = 6000
+hours = seconds // 3600
+minutes = seconds // 60
+print(f"{seconds} seconds is {hours} hours and {minutes} in minutes")
+print(f"-----------------------------------------------------------------")
 
+angle = 35
+sine = math.sin(angle)
+cosine = math.cos(angle)
 
+sine = round(sine, 1)
+cosine = round(cosine, 1)
 
+print(sine)
+print(cosine)
 
+print(f"Angle {angle} sine is {sine}")
+print(f"Angle {angle} cosine is {cosine}")
+print(f"--------------------------------------------------------------------")
 
+n = 4
+lots_of_heys = "hey" * n
+print(lots_of_heys)
+print(f"----------------------------------------------------------------------------")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+num_a = "3"
+num_b = "5"
+string_numbers = num_a + num_b
+print(string_numbers)
 
 
