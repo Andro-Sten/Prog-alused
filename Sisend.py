e5e93a9865ac365edd661d66a4a11e1948f2318f
Andro Sten <andro-sten.kriisa@voco.ee>
2022-10-27 16:29:53 +0300

    Ülesanne funktsioon

--- Sisend.py
+++ Sisend.py
@@ -2,4 +2,13 @@ nimi = str(input("Sisestage oma nimi: "))
 lubatud = str(input("Sisestage lubatud kiirus: "))
 tegelik = str(input("Sisestage tegelik kiirus: "))
 summa = (int(tegelik) - int(lubatud)) * 3
+summa = min(summa, 190)
 print(f"{nimi} kiiruse ületamise trahv on {summa} euri")
+
+
+
+
+
+
+
+
---
+++ Ülesanne Funktsioon.py
@@ -0,0 +1,31 @@
+# 1. Inside the functon.
+
+def func():
+    print(" I´m inside the function")
+
+# 2. My name is . . .
+
+def my_name_is(name):
+    print(f"My name is {name}")
+my_name_is("Ooga Booga")
+
+# 3. Sum six
+
+def sum_six(num):
+    return (6 + num)
+print(sum_six(5))
+
+# 4. Sum numbers
+def sum_numbers(a, b):
+    return (a + b)
+print(sum_numbers(5, 5))
+
+# 5. USD to EUR
+
+def usd_to_eur(dollar):
+    return round(dollar * 0.8)
+print(usd_to_eur(100))
+
+
+
+
